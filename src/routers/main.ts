7a6db9aad1a0f363af46a51d93a8052dc4db490e
Bruhtek <bartosz.kas2005@gmail.com>
2023-05-21 22:40:37 +0200

    Various fixes, add renderer to express

--- src/routers/main.ts
+++ src/routers/main.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getBattery } from "./api";
+import { getBattery, setBattery } from "./api";
 import { getHourlyWeatherForecast, getSolarData, getWeatherForecast } from "../api/weather";
 import { getLightStatus } from "../api/hass";
 import { getCalendarEvents } from "../api/caldav";
+import puppeteer from "puppeteer";
 
 const mainRouter = Router();
 
@@ -27,12 +28,57 @@ mainRouter.get("/vertical", (req, res) => {
 
 mainRouter.get("/page/vertical", async (req, res) => {
 
-	return res.render("page/vertical", {
-		battery: getBattery(),
-		forecast: await getHourlyWeatherForecast(),
-		solarData: await getSolarData(),
-		calendar: await getCalendarEvents(),
-	});
+	try {
+		const calendar = await getCalendarEvents();
+
+		return res.render("page/vertical", {
+			battery: getBattery(),
+			forecast: await getHourlyWeatherForecast(),
+			solarData: await getSolarData(),
+			calendar: calendar,
+		});
+	} catch (e) {
+		return res.render("page/vertical", {
+			battery: getBattery(),
+			forecast: await getHourlyWeatherForecast(),
+			solarData: await getSolarData(),
+			calendar: {},
+		});
+	}
+});
+
+mainRouter.get("/render/vertical", async (req, res) => {
+	const battery = req.query.battery;
+	if (battery) {
+		setBattery(Number(battery));
+	}
+
+	try {
+		const browser = await puppeteer.launch({
+			defaultViewport: {
+				width: 536,
+				height: 724,
+				deviceScaleFactor: 2,
+			},
+			headless: "new",
+		});
+
+		const page = await browser.newPage();
+		await page.goto("http://localhost:3000/page/vertical", {
+			waitUntil: "networkidle0",
+			timeout: 0,
+		});
+		const buffer = await page.screenshot({
+			type: "png",
+		});
+
+		await browser.close();
+
+		res.set("Content-Type", "image/png").send(buffer);
+	} catch (e) {
+		console.log(e);
+		res.status(500).end();
+	}
 });
 
 export default mainRouter;
